90481bb0aee9c252332e6781230f5b0b76e20f97
Kevin Piette <kevin@termina.org>
2020-09-05 11:27:15 +0200

    - Updated netstandard version and skiasharp
    - Misc improvements to Ixen Windows backend

--- Ixen.Windows/_Base/IxenApplication.cs
+++ Ixen.Windows/_Base/IxenApplication.cs
@@ -43,19 +43,27 @@ namespace Ixen.Windows
 
         protected override void Invalidate()
         {
-            _window.Invalidate(true);
+            _window?.Invalidate(true);
         }
 
         public override int Width
         {
             get => _window?.GetClientRect().Width ?? -1;
-            set => _window?.SetSize(value, Height);
+            set
+            {
+                _window?.SetSize(value, Height);
+                _scene.Width = value;
+            }
         }
 
         public override int Height
         {
             get => _window?.GetClientRect().Height ?? -1;
-            set => _window?.SetSize(Width, value);
+            set
+            {
+                _window?.SetSize(Width, value);
+                _scene.Height = value;
+            }
         }
 
         public override string Title
--- Ixen.Windows/_Base/IxenWindow.cs
+++ Ixen.Windows/_Base/IxenWindow.cs
@@ -13,6 +13,7 @@ namespace Ixen.Windows
         private readonly NativePixelBuffer _pixelBuffer;
         private bool _painted;
         private IntPtr _hdc;
+        private SKImageInfo _imageInfo;
         private SKSurface _surface;
         private Action<SKCanvas, SKRect> _renderer;
 
@@ -32,19 +33,12 @@ namespace Ixen.Windows
             User32Methods.GetClientRect(packet.Hwnd, out Rectangle clientRect);
             _pixelBuffer.EnsureSize(clientRect.Width, clientRect.Height);
             _hdc = User32Methods.BeginPaint(packet.Hwnd, out PaintStruct ps);
+            _imageInfo = new SKImageInfo(clientRect.Width, clientRect.Height, SKColorType.Bgra8888, SKAlphaType.Premul);
             _painted = false;
 
             try
             {
-                using
-                (
-                    _surface = SKSurface.Create
-                    (
-                        clientRect.Width, clientRect.Height,
-                        SKColorType.Bgra8888, SKAlphaType.Premul,
-                        _pixelBuffer.Handle, _pixelBuffer.Stride
-                    )
-                )
+                using(_surface = SKSurface.Create(_imageInfo, _pixelBuffer.Handle, _pixelBuffer.Stride))
                 {
                     if (_surface != null)
                     {
@@ -56,7 +50,10 @@ namespace Ixen.Windows
             finally
             {
                 if (_painted)
+                {
                     Gdi32Helpers.SetRgbBitsToDevice(_hdc, clientRect.Width, clientRect.Height, _pixelBuffer.Handle);
+                }
+
                 User32Methods.EndPaint(packet.Hwnd, ref ps);
             }
         }
